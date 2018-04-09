f4fe36ecf29881180db76f1763c056faaacbdcff
prodev880110 <jack.dev880110@gmail.com>
2018-04-09 11:07:50 -0400

    update
    comment server dev material and set up local

--- index.js
+++ index.js
@@ -2,35 +2,38 @@ const Blockchain = require('./chain/blockchain.js');
 const Transaction = require('./chain/transaction.js');
 const leeusNode = require('./chain/node.js');
 
-const port = 18070+Math.floor(Math.random()*30);
-console.log('starting node on ', port)
-let node1 = new leeusNode(port);
 
-node1.init();
+Leeus = new Blockchain();
 
-const http_port = 4000 + Math.floor(Math.random()*10);
-
-let BrewHTTP = function (){
-	const app = new express();
-
-	app.use(bodyParser.json());
-
-	app.get('/addNode/:port', (req, res)=>{
-		console.log('add host: '+req.params.port)
-		node1.addPeer('localhost', req.params.port)
-
-		res.send();
-	})
-
-	app.get('/spawnBrew/:teammember', (req, res)=>{
-		let newBlock = node1.createBlock(req.params.teammember);
-		console.log('block created');
-		res.send();
-	})
-
-	app.listen(http_port, () => {
-		console.log(`http server up.. ${http_port}`);
-	})
-}
-
-let httpserver = new BrewHTTP();
+// const port = 18070+Math.floor(Math.random()*30);
+// console.log('starting node on ', port)
+// let node1 = new leeusNode(port);
+//
+// node1.init();
+//
+// const http_port = 4000 + Math.floor(Math.random()*10);
+//
+// let BrewHTTP = function (){
+// 	const app = new express();
+//
+// 	app.use(bodyParser.json());
+//
+// 	app.get('/addNode/:port', (req, res)=>{
+// 		console.log('add host: '+req.params.port)
+// 		node1.addPeer('localhost', req.params.port)
+//
+// 		res.send();
+// 	})
+//
+// 	app.get('/spawnBrew/:teammember', (req, res)=>{
+// 		let newBlock = node1.createBlock(req.params.teammember);
+// 		console.log('block created');
+// 		res.send();
+// 	})
+//
+// 	app.listen(http_port, () => {
+// 		console.log(`http server up.. ${http_port}`);
+// 	})
+// }
+//
+// let httpserver = new BrewHTTP();
